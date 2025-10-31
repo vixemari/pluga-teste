a9cd86b1194fc275baf8c244cc58dfc50602064b
vixemari <marianarocha308@gmail.com>
2025-10-31 08:59:24 -0300

    feat: implementa a chamada a api

---
+++ src/api/appService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'https://pluga.co/ferramentas_search.json';
+
+export const fetchApps = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar ferramentas:', error);
+    throw new Error('Não foi possível carregar a lista de ferramentas.');
+  }
+};
